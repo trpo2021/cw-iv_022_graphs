7725c20d29926857e5cf5d986e8648cf3968cae2
JuliaPech <DrakulinaJ@yandex.ru>
2021-05-24 20:30:31 +0700

    Made a logical separation of functions

--- src/libgraphs/function.c
+++ src/libgraphs/function.c
@@ -1,4 +1,4 @@
-#include "graphs.h"
+#include "lib_for_cities.h"
 
 int get_rand(int min, int max) {
   return (double)rand() / (RAND_MAX + 1.0) * (max - min) + min;
@@ -35,14 +35,6 @@ int arguments_check(char **argv) {
     return -1;
   }
   return 0;
-}
-
-void graph_initialization(struct graph *g, int max_city) {
-  for (int i = 0; i < max_city; i++) {
-    for (int j = i; j < max_city; j++) {
-      graph_set_edge(g, i, j, get_rand(10, 30));
-    }
-  }
 }
 
 void cities_numbers(int *array_cities) {
@@ -230,85 +222,10 @@ void free_arrays(int *path_long, int *arr_length, int *arr_cities, int *path) {
   free(path_long);
 }
 
-void graph_clear(struct graph *g, int N) {
-  for (int i = 1; i < N - 1; i++) {
-    for (int j = 0; j < N; j++) {
-      g->m[i][j] = 0;
-    }
-  }
-}
-
-void graph_set_edge(struct graph *g, int i, int j, int w) {
-  if (i == j) {
-    g->m[i][j] = 0;
-  } else {
-    g->m[i][j] = w;
-    g->m[j][i] = w;
-  }
-}
-
-int graph_get_edge(struct graph *g, int i, int j) { return g->m[i][j]; }
-
 int memory_check(int *array) {
   if (array == NULL) {
     printf("Ошибка выделения памяти под массив\n");
     return -1;
   }
   return 0;
-}
-
-struct graph *graph_create(int nvertices) {
-  struct graph *g;
-  g = malloc(sizeof(*g));
-
-  if (g == NULL) {
-    printf("Ошибка выделения памяти под граф\n");
-    return NULL;
-  }
-
-  g->nvertices = nvertices;
-  g->m = malloc(sizeof(int *) * nvertices);
-
-  if (g->m == NULL) {
-    printf("Ошибка выделения памяти в графе\n");
-    free(g);
-    return NULL;
-  }
-
-  for (int i = 0; i < nvertices; i++) {
-    g->m[i] = malloc(sizeof(int) * nvertices);
-
-    if (g->m[i] == NULL) {
-      printf("Ошибка выделения памяти в графе\n");
-      free(g->m);
-      free(g);
-      return NULL;
-    }
-  }
-
-  g->visited = malloc(sizeof(int) * nvertices);
-
-  if (g->visited == NULL) {
-    printf("Ошибка выделения памяти под посещаемые вершины\n");
-    for (int i = 0; i < max_city; i++) {
-      free(g->m[i]);
-    }
-    free(g->m);
-    free(g);
-    return NULL;
-  }
-
-  graph_clear(g, nvertices);
-  return g;
-}
-
-void graph_free(struct graph *g, int max_city) {
-  free(g->visited);
-
-  for (int i = 0; i < max_city; i++) {
-    free(g->m[i]);
-  }
-
-  free(g->m);
-  free(g);
 }
