66f3edc799e57c07bcfe5c143698f155289b3859
HakkiSoul <alek.morozow2018@yandex.ru>
2021-05-27 17:32:00 +0700

    Formated test.c

--- test/test.c
+++ test/test.c
@@ -2,15 +2,15 @@
 #include "libgraphs/lib_for_cities.h"
 
 CTEST(argument_check, invalid_argument_1) {
-    char **argv = malloc(2 * sizeof(char*));
-    argv[0] = malloc(10 * sizeof(char));
-    argv[1] = malloc(4 * sizeof(char));
-    strcat(argv[0], "./graph");
-    strcat(argv[1], "-l");
+  char **argv = malloc(2 * sizeof(char *));
+  argv[0] = malloc(10 * sizeof(char));
+  argv[1] = malloc(4 * sizeof(char));
+  strcat(argv[0], "./graph");
+  strcat(argv[1], "-l");
 
-    int real = arguments_check(argv);
+  int real = arguments_check(argv);
 
-    int exp = -1;
+  int exp = -1;
 
-    ASSERT_EQUAL(exp, real);
+  ASSERT_EQUAL(exp, real);
 }
