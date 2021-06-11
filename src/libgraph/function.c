b43800946a4fd306bd9f53bcf31d8a1a782918b8
JuliaPech <DrakulinaJ@yandex.ru>
2021-06-11 22:17:19 +0700

    Simplifying the Dijkstra function

--- src/libgraph/function.c
+++ src/libgraph/function.c
@@ -36,6 +36,33 @@ void print_matrix(int nvert, int matrix[][nvert]) {
     printf("\n");
 }
 
+void get_min_minindex(int nvert, int *visit, int *d, int *min, int *minindex) {
+    for (int i = 0; i < nvert; i++) { 
+        if ((visit[i] == 1) && (d[i] < *min)) { 
+            *min = d[i];
+            *minindex = i;
+        }
+    }
+}
+
+int get_short_path(int nvert, int start, int end, int matrix[][nvert], int *d, int *ver, int preg, int weight) {
+
+    while (end != start) { // пока не дошли до начальной вершины
+        for (int i = 0; i < nvert; i++) { // просматриваем все вершины
+            if (matrix[i][end] != 0) {                                  // если связь есть
+                int temp = weight - matrix[i][end]; // определяем вес пути из предыдущей вершины
+                if (temp == d[i]) {                   // если вес совпал с рассчитанным, значит из этой вершины и был переход
+                    weight = temp;  // сохраняем новый вес
+                    end = i;        // сохраняем предыдущую вершину
+                    ver[preg] = i + 1; // и записываем ее в массив
+                    preg++;
+                }
+            }
+        }
+    }
+    return preg;
+}
+
 void dijkstra(int start, int end, int nvert, int matrix[][nvert]) {
     int d[nvert];        
     // минимальное расстояние
@@ -55,13 +82,8 @@ void dijkstra(int start, int end, int nvert, int matrix[][nvert]) {
         minindex = MAX_LENGTH_PATH;
         min = MAX_LENGTH_PATH;
 
-        for (int i = 0; i < nvert; i++) { 
-            if ((visit[i] == 1) && (d[i] < min)) { 
-                min = d[i];
-                minindex = i;
-            }
-        }
-
+        get_min_minindex(nvert, visit, d, &min, &minindex);
+        
         if (minindex != MAX_LENGTH_PATH) {
             for (int i = 0; i < nvert; i++) {
                 if (matrix[minindex][i] > 0) {
@@ -77,34 +99,12 @@ void dijkstra(int start, int end, int nvert, int matrix[][nvert]) {
 
     // Восстановление пути
     int ver[nvert];      
-    // массив посещенных вершин
+    // // массив посещенных вершин
     ver[0] = end + 1;    
-    int preg = 1;        
-    // индекс предыдущей вершины
-    int weight = d[end]; 
-    // вес конечной вершины
+    int preg = 1;        // индекс предыдущей вершины
+    int weight = d[end]; // вес конечной вершины
 
-    while (end != start) { 
-        // пока не дошли до начальной вершины
-        for (int i = 0; i < nvert; i++) { 
-            // просматриваем все вершины
-            if (matrix[i][end] != 0) {  
-                // если связь есть
-                int temp = weight - matrix[i][end]; 
-                // определяем вес пути из предыдущей вершины
-                if (temp == d[i]) {                   
-                    // если вес совпал с рассчитанным, значит из этой вершины и был переход
-                    weight = temp;  
-                    // сохраняем новый вес
-                    end = i;       
-                    // сохраняем предыдущую вершину
-                    ver[preg] = i + 1; 
-                    // и записываем ее в массив
-                    preg++;
-                }
-            }
-        }
-    }
+    preg = get_short_path(nvert, start, end, matrix, d, ver, preg, weight);
 
     //Вывод пути (начальная вершина оказалась в конце массива из k элементов)
     printf("\nВывод кратчайшего пути\n");
--- src/libgraph/library.h
+++ src/libgraph/library.h
@@ -8,3 +8,5 @@ int getdigit(char *str);
 int incorrect_input(int nvert, int start, int end);
 void print_matrix(int nvert, int matrix[][nvert]);
 void dijkstra(int start, int end, int nvert, int matrix[][nvert]);
+void get_min_minindex(int nvert, int *visit, int *d, int *min, int *minindex);
+int get_short_path(int nvert, int start, int end, int matrix[][nvert], int *d, int *ver, int preg, int weight);
