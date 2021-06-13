b13e19107f86f437fd8b859605b4402ae7d2216b
JuliaPech <DrakulinaJ@yandex.ru>
2021-06-14 00:24:51 +0700

    Correct some functions

--- src/libgraph/all_paths.h
+++ src/libgraph/all_paths.h
@@ -1,8 +1,9 @@
 #pragma once
 #define MAX_LENGTH_STR 200
 
-extern int all_paths_print(int src, int dst);
-extern int all_paths_free();
-extern int all_paths_init(int *g_adj, int g_adj_size, int aflag);
-
+void all_paths_print(int src, int dst);
+void all_paths_free();
+int all_paths_init(int *g_adj, int g_adj_size, int aflag);
+void all_paths_print_recur(int node, int dst, int visited[], int path[],
+                           int *path_index);
 int getdigit(char *str);
--- src/libgraph/graph.h
+++ src/libgraph/graph.h
@@ -1,6 +1,6 @@
 #pragma once
-extern int graph_adj_init(int size);
-extern int graph_adj_add(int row, int col);
-extern int graph_adj_print();
-extern int graph_adj_free();
-extern int *graph_adj_get();
+int graph_adj_init(int size);
+int graph_adj_add(int row, int col);
+int graph_adj_print();
+void graph_adj_free();
+int *graph_adj_get();
